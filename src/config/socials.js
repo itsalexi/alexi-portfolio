560dfcc8a0af9be7d538aab578d9293e85abe3b9
Alexi <108108603+itsalexi@users.noreply.github.com>
2025-12-27 23:49:58 +0800

    I FORGOT TO CHANGE THE LINK

--- src/config/socials.js
+++ src/config/socials.js
@@ -2,7 +2,7 @@ export const socials = {
   github: "https://github.com/itsalexi",
   linkedin: "https://linkedin.com/in/alexicanamo",
   instagram: "https://instagram.com/alexi_canamo",
-  discord: "https://discord.gg/YOUR_SERVER_INVITE",
+  discord: "https://discord.alexi.life",
   email: "alexicanamo@gmail.com",
   resume: "https://cv.alexi.life",
 };
